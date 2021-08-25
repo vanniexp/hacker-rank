46a9356a37f216bea7b7e627d20be362b05a3aa1
vanniexp <vaniacristina.s@live.com>
2021-08-25 20:49:46 -0300

    feat: :construction:

--- Solutions/timeConversion.js
+++ Solutions/timeConversion.js
@@ -22,15 +22,41 @@ function readLine() {
   return inputString[currentLine++];
 }
 
-/*
- * Complete the 'timeConversion' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts STRING s as parameter.
- */
-
 function timeConversion(s) {
+  var hours = parseInt(s.substring(0, s.indexOf(':')));
+  var minutes = parseInt(s.substring(s.indexOf(':') + 1, s.lastIndexOf(':')));
+  var seconds = parseInt(s.substring(s.lastIndexOf(':') + 1, s.lastIndexOf(':') + 3));
+
+  var militaryHours;
+
+  if (hours === 12) {
+      militaryHours = 0;
+  } else {
+      militaryHours = hours;
+  }
+
+  var hourShift; // Adds 12 hours for all input string containing PM
+  if(s.toLowerCase().indexOf('pm') > -1) {
+      hourShift = 12;
+  } else {
+      hourShift = 0;
+  }
+
+  function addZeroFormat(numString) {
+      if (numString.length === 1) {
+          return "0" + numString;
+      }
+      return numString;
+  }
+
+  var formattedHours = addZeroFormat((militaryHours + hourShift).toString());
+  var formattedMinutes = addZeroFormat(minutes.toString());
+  var formattedSeconds = addZeroFormat(seconds.toString());
+
+  return (formattedHours + ":" + formattedMinutes + ":" + formattedSeconds);
+
 }
+
 
 function main() {
   const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
